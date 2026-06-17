d8c15d4e12f85fd8db616617906802276444a848
Zaib KHAN <zkhan@intersysto.eu>
2026-06-17 02:42:33 +0200

    update scalafmt version

--- app/controllers/HealthController.scala
+++ app/controllers/HealthController.scala
@@ -5,8 +5,8 @@ import play.api.mvc.*
 
 @Singleton
 final class HealthController @Inject() (
-                                         cc: ControllerComponents
-                                       ) extends AbstractController(cc):
+    cc: ControllerComponents
+) extends AbstractController(cc):
 
   def health: Action[AnyContent] = Action:
     Ok("Lamora backend is running")
